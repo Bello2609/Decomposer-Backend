e455139e215ca3cae357b5148358f4c6aa4d8367
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-12-03 18:00:03 +0100

    :construction: File restructure

---
+++ src/api/routes/index.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const authRoutes = require("./auth");
+const router = Router();
+
+router.use("/auth", authRoutes);
+router.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    route: "Routes",
+    timestamp: Date.now(),
+  });
+});
+
+module.exports = router;
