a1a80f48413aaa1cc513ecbabee9273dd4d26ded
bello hadi <bellohadi82@gmail.com>
2022-12-04 20:09:48 +0100

    user schema is added

---
+++ src/api/model/Review.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const reviewSchema = new Schema({
+    
+})
+module.exports = mongoose.model("Review", reviewSchema);
--- src/api/model/User.js
+++ src/api/model/User.js
@@ -3,6 +3,44 @@ const { Schema }  = mongoose;
 
 
 const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    services: {
+        type: String,
+        required: true
+    },
+    profile_image: {
+        type: String,
+        required: true
+    },
+    header_image: {
+        type: String,
+        required: true
+    },
+    amount: {
+        type: Schema.Type.ObjectId,
+        ref: "Amount"
+        required: true
+    },
+    isVerified: {
+        type: Boolean,
+        required: true
+    }
+
 
 })
 module.exports = mongoose.model("User", userSchema);
