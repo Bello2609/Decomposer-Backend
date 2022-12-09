ffd8f48069d540c77cd3baea18491f043e8368bc
roqeebYusuff <roqeebyusuff001@gmail.com>
2022-12-09 10:53:53 +0100

    :art: Order CRUD completed

--- src/api/routes/Order/index.js
+++ src/api/routes/Order/index.js
@@ -1,11 +1,21 @@
 const { Router } = require("express");
-const { createOrder } = require("../../controller/Order");
+const {
+  createOrder,
+  myOrders,
+  oneOrder,
+  updateOrder,
+  deleteOrder,
+} = require("../../controller/Order");
 const { authorize } = require("../../middleware/authorize");
 const authorizeBuyerOnly = require("../../middleware/authorizeBuyerOnly");
 const router = Router();
 
-// router.post("/create", authorize, createOrder);
 router.post("/create", authorize, authorizeBuyerOnly, createOrder);
+router.get("/myOrders", authorize, authorizeBuyerOnly, myOrders);
+router.get("/oneOrder/:id", authorize, authorizeBuyerOnly, oneOrder);
+router.put("/updateOrder/:id", authorize, authorizeBuyerOnly, updateOrder);
+router.delete("/deleteOrder/:id", authorize, authorizeBuyerOnly, deleteOrder);
+
 router.get("/", (req, res) => {
   res.status(200).json({
     success: true,
